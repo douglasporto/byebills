b83b910da0e818ec2b4b8794f520b35bba2bd035
Douglas Porto <douglasalexandre7@gmail.com>
2021-05-11 19:15:16 -0300

    :bug: Fix: storybook with chakra

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,18 @@
 
-import GlobalStyles from '../src/styles/global'
-export const decorators = [
-  (Story) => (
-    <>
-      <GlobalStyles />
-      <Story />
-    </>
-  ),
-];
+import { ChakraProvider, Flex } from '@chakra-ui/react'
+import { addDecorator } from "@storybook/react";
+import { theme } from '../src/styles/theme'
+
+addDecorator(story => (
+  <ChakraProvider theme={theme}>
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      flexDirection="column"
+    >
+      {story()}
+    </Flex>
+  </ChakraProvider>
+))
--- src/components/Form/Input/stories.tsx
+++ src/components/Form/Input/stories.tsx
@@ -1,9 +1,38 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import { Input } from '.'
+import { InputProps, Input } from './Input'
+import { Flex } from '@chakra-ui/core'
 
 export default {
   title: 'Input',
-  component: Input
+  component: Input,
+  argTypes: {
+    name: {
+      type: 'string'
+    },
+    label: {
+      type: 'string'
+    }
+  }
 } as Meta
 
-export const Default: Story = () => <Input />
+const Template: Story<InputProps> = ({ ...args }) => {
+  return (
+    <Flex
+      as="form"
+      width="100%"
+      maxWidth={360}
+      backgroundColor="gray.800"
+      padding="8"
+      borderRadius={8}
+      flexDirection="column"
+    >
+      <Input name={args.name} label={args.label} />
+    </Flex>
+  )
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  name: 'email',
+  label: 'E-mail'
+}
